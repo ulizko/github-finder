f51489a115429789ba6bdde5656dfb64a67ad3a6
Oleksandr Ulizko <au@wizardry.ua>
2019-07-16 11:55:07 +0300

    add home and not found components

--- src/context/github/GithubState.js
+++ src/context/github/GithubState.js
@@ -10,12 +10,23 @@ import {
   GET_REPOS,
 } from '../types';
 
+let githubClientId;
+let githubClientSecret;
+
+if (process.env.NODE_ENV !== 'production') {
+  githubClientId = process.env.REACT_APP_GITHUB_CLIENT_ID;
+  githubClientSecret = process.env.REACT_APP_GITHUB_CLIENT_SECRET;
+} else {
+  githubClientId = process.env.GITHUB_CLIENT_ID;
+  githubClientSecret = process.env.GITHUB_CLIENT_SECRET;
+}
+
 const GithubState = props => {
   const baseUrl = `https://api.github.com`;
 
   const credentials = {
-    client_id: process.env.REACT_APP_GITHUB_CLIENT_ID,
-    client_secret: process.env.REACT_APP_GITHUB_CLIENT_SECRET,
+    client_id: githubClientId,
+    client_secret: githubClientSecret,
   };
 
   const initialState = {
